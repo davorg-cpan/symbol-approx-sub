4b95c517281d0f0712ec8252e139613333781d84
danielksan81 <danielksan81@gmail.com>
2016-10-06 08:29:32 +0200

    Removed SVN information from files (#8)
    * fixes #3

--- lib/Symbol/Approx/Sub/String/Equal.pm
+++ lib/Symbol/Approx/Sub/String/Equal.pm
@@ -1,24 +1,12 @@
 #
 # Symbol::Approx::Sub::String::Equal
 #
-# $Id$
-#
 # Matcher plugin for Symbol::Approx::Sub;
 #
 # Copyright (c) 2000, Magnum Solutions Ltd. All rights reserved.
 #
 # This module is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
-#
-# $Log$
-# Revision 1.2  2005/10/22 17:56:22  dave
-# Added docs.
-#
-# Revision 1.1  2002/03/29 17:55:31  dave
-# Support modules
-#
-# Revision 1.1  2001/07/15 20:43:10  dave
-# Initial revision
 #
 #
 package Symbol::Approx::Sub::String::Equal;
